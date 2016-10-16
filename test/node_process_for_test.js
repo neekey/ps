56c3792ce2d7d10052972e27ec1cf58e3fe274b7
neekey <ni184775761@gmail.com>
2016-10-16 16:23:52 +1100

    Add .editorconfig and reformat code

--- test/node_process_for_test.js
+++ test/node_process_for_test.js
@@ -1,5 +1,5 @@
 var now = Date.now();
-setTimeout(function(){
-    var interval = Date.now() - now;
-    console.log( 'Timer finished, time consuming: ' + interval );
-}, 1000000 );
+setTimeout(function () {
+  var interval = Date.now() - now;
+  console.log('Timer finished, time consuming: ' + interval);
+}, 1000000);
